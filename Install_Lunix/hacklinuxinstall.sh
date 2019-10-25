ebf247de9614cac5a8f6bac221e6ba64df622375
Candyman <46049229+TheManInTheMiddle@users.noreply.github.com>
2019-10-25 10:43:32 +0200

    Create hacklinuxinstall.sh

---
+++ Install_Lunix/hacklinuxinstall.sh
@@ -0,0 +1,30 @@
+#UPDATE
+apt update
+apt upgrade
+
+#OS setup
+alias ll='ls -l'
+
+#Tools
+mkdir ./tools
+cd ./tools
+#cupp.py
+git clone https://github.com/Mebus/cupp.git
+#scrapy
+pip install scrapy
+#responder/multirelay
+git clone https://github.com/camopants/igandx-Responder.git
+#mimikatz
+git clone https://github.com/gentilkiwi/mimikatz.git
+#unicorn
+git clone https://github.com/trustedsec/unicorn.git
+#fatrat
+git clone https://github.com/Screetsec/TheFatRat.git
+#empire
+git clone https://github.com/EmpireProject/Empire.git
+#seth
+git clone https://github.com/SySS-Research/Seth.git
+#WES
+git clone https://github.com/bitsadmin/wesng.git
+#LES
+git clone https://github.com/mzet-/linux-exploit-suggester.git
